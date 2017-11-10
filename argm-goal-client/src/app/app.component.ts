f767c0dc0df1db33bf62e9bb5b29208f8e000143
Storm Burpee <stormburpee@gmail.com>
2017-11-10 20:50:28 +1030

    starting development

--- argm-goal-client/src/app/app.component.ts
+++ argm-goal-client/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './page/header/header.component';
+
+import { RouterModule, Routes, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
--- argm-goal-client/src/app/app.module.ts
+++ argm-goal-client/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './page/header/header.component';
+import { HomeComponent } from './routes/home/home.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { MatToolbarModule, MatToolbar } from '@angular/material';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
